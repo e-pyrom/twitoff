d20f2a8d255593609de53f52a40734ba1f56fa3b
Pyrom E <e.pyrom@gmail.com>
2020-05-02 22:18:08 -0400

    Final Commit

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,24 +1,35 @@
 # web_app/__init__.py
 
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
+from web_app.routes.admin_routes import admin_routes
+from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URI = "sqlite:///web_app_99.db" # using relative filepath
-#DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
-#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY # enable flash messaging via sessions
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(twitter_routes)
+    app.register_blueprint(admin_routes)
+    app.register_blueprint(stats_routes)
 
     return app
 
--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,11 +1,65 @@
 # web_app/routes/twitter_routes.py
 
-from flask import Blueprint, jsonify#, render_template, 
-#from web_app.services.twitter_service import twitter_api_client
+from flask import Blueprint, jsonify #, render_template, request, flash, redirect
+
+from web_app.models import db, User, Tweet, parse_records
+from web_app.services.twitter_service import api as twitter_api
+from web_app.services.basilica_service import connection as basilica_connection
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
 @twitter_routes.route("/users/<screen_name>/fetch")
 def fetch_user_data(screen_name):
-    print(screen_name)
-    return f"Fetched {screen_name} OK"
+    print("FETCHING...", screen_name)
+
+    #
+    # fetch user info
+    #
+    user = twitter_api.get_user(screen_name)
+
+    #
+    # store user info in database
+    #
+
+    db_user = User.query.get(user.id) or User(id=user.id)
+    db_user.screen_name = user.screen_name
+    db_user.name = user.name
+    db_user.location = user.location
+    db_user.followers_count = user.followers_count
+    db.session.add(db_user)
+    db.session.commit()
+
+    #
+    # fetch their tweets
+    #
+
+    statuses = twitter_api.user_timeline(screen_name, tweet_mode="extended", count=150)
+    print("STATUSES", len(statuses))
+
+    #
+    # fetch embedding for each tweet
+    #
+
+    tweet_texts = [status.full_text for status in statuses]
+    embeddings = list(basilica_connection.embed_sentences(tweet_texts, model="twitter"))
+    print("EMBEDDINGS", len(embeddings))
+
+    #
+    # store tweets in database (w/ embeddings)
+    #
+
+    for index, status in enumerate(statuses):
+        print(status.full_text)
+        print("----")
+        db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
+        db_tweet.user_id = status.author.id
+        db_tweet.full_text = status.full_text
+        embedding = embeddings[index]
+        print(len(embedding))
+        db_tweet.embedding = embedding
+        db.session.add(db_tweet)
+
+    db.session.commit()
+
+    return f"FETCHED {screen_name} OK"
+    #return jsonify({"user": user._json, "num_tweets": len(statuses)})
